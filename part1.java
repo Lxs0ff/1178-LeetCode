8c0bb7e494e0e47097724ae2e03d1c42db390400
Lxs_ <112419013+Lxs0ff@users.noreply.github.com>
2024-11-18 13:11:32 -0500

    yippe

--- part1.java
+++ part1.java
@@ -2,23 +2,28 @@ class Solution {
     public List<Integer> findNumOfValidWords(String[] words, String[] puzzles) {
         List<Integer> ans = new ArrayList<>();
         for (int p = 0; p < puzzles.length;p++) {
-            List<Character> chars = new ArrayList<>();
             int count = 0;
+            Boolean condition = false;
             for (int w = 0; w < words.length;w++) {
+                List<Character> chars = new ArrayList<>();
                 for (int i = 0; i < words[w].length();i++) {
+                    if (words[w].charAt(i) == puzzles[p].charAt(0)){
+                        condition = true;
+                    }
                     if (!chars.contains(words[w].charAt(i))){
                         chars.add(words[w].charAt(i));
                     }
                 }
                 System.out.println(chars);
+                System.out.println(condition);
                 String puzzle = puzzles[p];
                 for (int q = 0; q < puzzle.length(); q++) {
                     if (chars.contains(puzzle.charAt(q))){
                         chars.remove(chars.indexOf(puzzle.charAt(q)));
                     }
                 }
-                if (chars.size() == 0) {
-                    condition2 = true;
+                if (chars.size() == 0 && condition) {
+                    count++;
                 }
             }
             ans.add(count);
