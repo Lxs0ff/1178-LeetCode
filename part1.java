2e731fdaea9b97f9cc9304a786ef3ae320f04c56
Lxs_ <112419013+Lxs0ff@users.noreply.github.com>
2024-11-18 13:26:59 -0500

    Sa marche juste pas asser vite

--- part1.java
+++ part1.java
@@ -3,26 +3,20 @@ class Solution {
         List<Integer> ans = new ArrayList<>();
         for (int p = 0; p < puzzles.length;p++) {
             int count = 0;
-            Boolean condition = false;
             for (int w = 0; w < words.length;w++) {
                 List<Character> chars = new ArrayList<>();
                 for (int i = 0; i < words[w].length();i++) {
-                    if (words[w].charAt(i) == puzzles[p].charAt(0)){
-                        condition = true;
-                    }
                     if (!chars.contains(words[w].charAt(i))){
                         chars.add(words[w].charAt(i));
                     }
                 }
-                System.out.println(chars);
-                System.out.println(condition);
                 String puzzle = puzzles[p];
                 for (int q = 0; q < puzzle.length(); q++) {
                     if (chars.contains(puzzle.charAt(q))){
                         chars.remove(chars.indexOf(puzzle.charAt(q)));
                     }
                 }
-                if (chars.size() == 0 && condition) {
+                if (chars.size() == 0 && words[w].indexOf(puzzles[p].charAt(0)) != -1) {
                     count++;
                 }
             }
